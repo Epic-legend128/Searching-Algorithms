58faf13ccd6493c044d5fdffcc6d120988a0098a
Epic-legend128 <100092577+Epic-legend128@users.noreply.github.com>
2022-03-10 10:57:35 +0200

    Update Binary-Search.cpp

--- C++/Binary-Search.cpp
+++ C++/Binary-Search.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 //binary search
 int binary(const std::vector<int>& array, int key) {
   int right = array.size()-1;
   int left = 0;
   while (left <= right) {
-    int mid = right+left/2;
+    int mid = floor(right+left/2);
     if (array[mid] < key) {
       left = mid+1;
     }
