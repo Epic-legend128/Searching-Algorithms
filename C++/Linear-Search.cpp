177b5cbe93a84e0409645251ea18d6e7b2eb345d
Epic-legend128 <100092577+Epic-legend128@users.noreply.github.com>
2022-03-10 08:31:11 +0200

    Update Linear-Search.cpp

--- C++/Linear-Search.cpp
+++ C++/Linear-Search.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <vector>
 
+//linear search
 int linear(const vector<int>& array, key) {
   for (int i = 0; i<array.length(); i++) {
-    if (array[i] === key) {
+    if (array[i] == key) {
       return i;
     }
   }
@@ -11,7 +12,26 @@ int linear(const vector<int>& array, key) {
 }
 
 int main() {
-  int limit = 10;
-  vector<int> myArray(limit, 0);
+  int size = 5;
+  vector<int> nums(size, 0);
+  cout << "Enter "<<size<<" numbers:\n";
+
+  for (int i = 0; i < size; i++) {
+    int current;
+    cin >> current;
+    nums.push_back(current);
+  }
+  
+  cout << "Type which number you arelooking for in the array:\n";
+  cin >> key;
+  
+  int index = linear(nums, key);
+  if (index == NULL) {
+    cout << "Couldn't find the number you were looking for.\n";
+  }
+  else {
+    cout << "The number you are looking for is at index "<<index<<" of the array\n";
+  }
+  
   return 0;
 }
