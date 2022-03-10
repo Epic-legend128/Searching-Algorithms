702d1cb21338a5f75dbec6238c936b3035e59a86
Epic-legend128 <100092577+Epic-legend128@users.noreply.github.com>
2022-03-10 08:32:42 +0200

    Update Linear-Search.cpp

--- C++/Linear-Search.cpp
+++ C++/Linear-Search.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 //linear search
-int linear(const vector<int>& array, key) {
+int linear(const std::vector<int>& array, key) {
   for (int i = 0; i<array.length(); i++) {
     if (array[i] == key) {
       return i;
@@ -13,24 +13,24 @@ int linear(const vector<int>& array, key) {
 
 int main() {
   int size = 5;
-  vector<int> nums(size, 0);
-  cout << "Enter "<<size<<" numbers:\n";
+  std::vector<int> nums(size, 0);
+  std::cout << "Enter "<<size<<" numbers:\n";
 
   for (int i = 0; i < size; i++) {
     int current;
-    cin >> current;
+    std::cin >> current;
     nums.push_back(current);
   }
   
-  cout << "Type which number you arelooking for in the array:\n";
-  cin >> key;
+  std::cout << "Type which number you arelooking for in the array:\n";
+  std::cin >> key;
   
   int index = linear(nums, key);
   if (index == NULL) {
-    cout << "Couldn't find the number you were looking for.\n";
+    std::cout << "Couldn't find the number you were looking for.\n";
   }
   else {
-    cout << "The number you are looking for is at index "<<index<<" of the array\n";
+    std::cout << "The number you are looking for is at index "<<index<<" of the array\n";
   }
   
   return 0;
